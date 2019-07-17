d6c93203ee671a2a7a022e0a779befc38b55a677
木丶屋 <940727683@qq.com>
2019-07-17 15:41:56 +0800

    商品列表

--- src/main.js
+++ src/main.js
@@ -9,6 +9,16 @@ import './assets/css/global.css'
 //
 import axios from 'axios'
 
+//导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+
+// require styles 导入富文本编辑器对应得样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+//导入Nprogress
+
 //导入组件
 import TreeTable from 'vue-table-with-tree-grid'
 
@@ -24,7 +34,27 @@ axios.interceptors.request.use(function(config) {
     //将axios 添加到原型中
 Vue.config.productionTip = false
 
+
+
+
 Vue.component('tree-table', TreeTable)
+    // 将富文本编辑器 注册为全局可用得组件
+Vue.use(VueQuillEditor)
+
+Vue.filter('dateFormat', function(originVal) {
+    const dt = new Date(originVal)
+
+    const y = dt.getFullYear()
+    const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+    const d = (dt.getDate() + '').padStart(2, '0')
+
+    const hh = (dt.getHours() + '').padStart(2, '0')
+    const mm = (dt.getMinutes() + '').padStart(2, '0')
+    const ss = (dt.getSeconds() + '').padStart(2, '0')
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
+
 
 
 new Vue({
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -33,7 +33,11 @@ import {
     Alert,
     Tabs,
     TabPane,
-
+    Steps,
+    Step,
+    Checkbox,
+    CheckboxGroup,
+    Upload
 } from 'element-ui'
 
 
@@ -68,6 +72,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(Checkbox)
+Vue.use(CheckboxGroup)
+Vue.use(Upload)
     // 进行全局挂载：VUE实例上
 Vue.prototype.$message = Message;
 Vue.prototype.$confirm = MessageBox.confirm
